a9d2c23060b3506fa6f92a530e73a9d069e518a4
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-08-27 16:26:45 -0400

    Add project files.

---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassProject {
+    class Student {
+
+        public string Firstname { get; set;  }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Finalgrade { get; set; }
+
+        public string GetFullname() {
+            return Firstname + " " + Lastname;
+        }
+        public void SetFinalGrade(int finalgrade) {
+            if (finalgrade < 0) {
+                Console.WriteLine("ERROR: Grade must be greater than 0");
+            } else {
+            Finalgrade = finalgrade;
+               
+            }
+        }
+    }
+
+}
